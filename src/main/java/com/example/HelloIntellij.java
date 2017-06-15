f546669840bb1b5fb10756ae7146896b0d6af0f3
niejq <niejq@1dian.la>
2017-06-15 15:10:54 +0800

    优化

--- src/main/java/com/example/HelloIntellij.java
+++ src/main/java/com/example/HelloIntellij.java
@@ -15,13 +15,17 @@ public class HelloIntellij {
 
     public static void main(String[] args) {
         try {
+            //连接池配置
             JedisPoolConfig config = new JedisPoolConfig();
             config.setMaxTotal(30);
             config.setMaxIdle(10);
             config.setTestOnBorrow(true);
             config.setTestOnReturn(true);
+            //创建连接池
             JedisPool pool = new JedisPool(config,"192.168.30.128",6379,1500,"laonie");
+            //获取jedis实例
             Jedis jedis = pool.getResource();
+            //调用操作api，返回操作结果
             String username = jedis.get("username");
             System.out.println(username);
         } catch (Exception e) {
